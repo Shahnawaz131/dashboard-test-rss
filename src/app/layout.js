cffec0f5766b2a1003bbd6e18642c582863891fa
Shahnawaz Ali <shahnawaz132007@gmail.com>
2025-06-28 14:20:35 +0530

    fixed import file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from 'next/font/google'
 import "./globals.css"
-import ClientLayout from "./ClientLayout"
+import ClientLayout from "./clientLayout"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
